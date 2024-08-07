4a10e6f95a6ce2e3137751fa10c12a2caf95b594
Thejas <teju270934@gmail.com>
2024-08-07 16:33:40 +0530

    now done with cat command implemetnation

---
+++ systemCallPrograms/cat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cat_file(const char *path)
+{
+      FILE *file = fopen(path, "r");
+
+      // edge case
+      if (file == NULL)
+      {
+            perror("fopen");
+            exit(EXIT_FAILURE);
+      }
+
+      char ch;
+      while ((ch = fgetc(file)) != EOF)
+            putchar(ch);
+
+      fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+      // edge case
+      if (argc < 2)
+      {
+            fprintf(stderr, "usage: %s <filepath>...\n", argv[0]);
+            exit(EXIT_FAILURE);
+      }
+
+      for (int i = 1; i < argc; i++)
+      {
+            printf("Content of %s:\n", argv[i]);
+            cat_file(argv[i]);
+      }
+
+      return 0;
+}
