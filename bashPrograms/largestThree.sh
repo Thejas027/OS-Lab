680dbd17fea6669faad55631afdfeaace97d1916
Thejas <teju270934@gmail.com>
2024-08-07 16:49:36 +0530

    added bask program to find the a largest of three numbers

---
+++ bashPrograms/largestThree.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo "Enter three number to find the largest of those : "
+read  num1 num2 num3 
+
+if [ $num1 -gt $num2 ]  && [ $num1 -gt $num3 ]; then 
+      echo "$num1 is the greatest."
+elif [ $num2 -gt $num1 ] && [ $num2 > $num3 ]; then 
+      echo "$num2 is the greatest."
+else
+      echo "$num3 is the greatest."
+fi
+
